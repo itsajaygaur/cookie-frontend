eb358b8f07062b7729fc9671bdae3a5b37833a5b
ajay <ajaypathak2527@gmail.com>
2024-11-22 23:52:14 +0530

    credentials include added

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -14,9 +14,11 @@ const {token} = useToken()
 async function getData() {
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/profile`, {
-            headers: {
-                Cookie: `token=${token}`,
-            },
+            // headers: {
+            //     Cookie: `token=${token}`,
+            //     // 'x-data': 'wow'
+            // },
+            credentials: 'include'
         });
         const data = await response.json();
         console.log('received response',data)
@@ -39,7 +41,7 @@ console.log('token provider -> ',token)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dashboard</h1>
+      <h1>client side</h1>
         <p>{text || "unexpected error"}</p> 
     </main>
   );
